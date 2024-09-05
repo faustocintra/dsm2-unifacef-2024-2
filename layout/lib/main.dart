c759e4f1dd0c718c4df9439882d9f12eaf414628
robertopapacidero <robertopapacidero@gmail.com>
2024-09-05 23:34:30 +0000

    (05/09) termino do projeto layout

--- layout/lib/main.dart
+++ layout/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+//fonts.google.com/icons
+//materialpalette.com/colors
 void main() {
   runApp(const MainApp());
 }
@@ -24,7 +25,8 @@ class Layout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(children: [
-      LightBlueBox(),
+      Stacked(),
+      InfoPanel(),
       IconsRow()
     ],);
   }
@@ -56,5 +58,67 @@ class LightBlueBox  extends StatelessWidget {
       width: double.infinity, //largura total
       color: Colors.lightBlue[500]
     );
+  }
+}
+
+class IndigoBox extends StatelessWidget {
+  const IndigoBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150,
+      width: 320,
+      color: Colors.indigo.shade800,
+    );
+  }
+}
+
+class Logo extends StatelessWidget {
+  const Logo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network('https://www.unifacef.com.br/wp-content/uploads/2016/02/logo-branco.png');
+  }
+}
+
+class Stacked extends StatelessWidget {
+  const Stacked({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //Stack exibe seus filhos (children) empilhados uns sobre os outros
+    return const Stack(
+      alignment: Alignment.center,
+      children: [LightBlueBox(), IndigoBox(), Logo()]);
+  }
+}
+
+class InfoPanel extends StatelessWidget {
+  const InfoPanel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Expanded ocupa todo o espaço disponível na vertical
+    return Expanded(
+        child: Container(
+            color: Colors.yellow[200], // ou: Colors.yellow.shade200
+            padding: const EdgeInsets.all(20),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Exemplo de layout Flutter",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.indigo.shade600,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold)),
+                  const Text("Desenvolvido por Fausto G. Cintra",
+                      textAlign: TextAlign.right,
+                      style:
+                          TextStyle(fontStyle: FontStyle.italic, fontSize: 16))
+                ])));
   }
 }
