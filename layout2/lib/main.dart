76838112890c987bbc0c84bed18361c44d008549
luizverlu <luizverlu@gmail.com>
2024-09-12 23:31:11 +0000

    construção do layout 2

--- layout2/lib/main.dart
+++ layout2/lib/main.dart
@@ -6,28 +6,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -36,90 +20,134 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Layout());
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Layout extends StatelessWidget {
+  const Layout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return Column(
+      children: [
+        // Primeira linha - Nome centralizado
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.brown[400],
+                height: 150,
+                child: const Center(
+                  child: Text(
+                    'Luiz Mario Faleiros',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        // Segunda linha - Data da prova
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.blue[400],
+                height: 150,
+                child: const Center(
+                  child: Text(
+                    '19/09/2024',
+                    style: TextStyle(
+                      color: Colors.orangeAccent,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        // Linhas com 4 colunas
+        Expanded(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // Primeira coluna com dois ícones
+              Expanded(
+                child: Container(
+                  color: Colors.purple[400],
+                  child: const Column(
+                    children: [
+                      Icon(Icons.home, size: 50),
+                      Spacer(),
+                      Icon(Icons.settings, size: 50),
+                    ],
+                  ),
+                ),
+              ),
+              // Segunda coluna com ícone
+              Expanded(
+                child: Container(
+                  color: Colors.pink[400],
+                  child: const Center(
+                    child: Icon(Icons.phone, size: 50),
+                  ),
+                ),
+              ),
+              // Terceira coluna com ícone
+              Expanded(
+                child: Container(
+                  color: Colors.green[400],
+                  child: const Center(
+                    child: Icon(Icons.camera, size: 50),
+                  ),
+                ),
+              ),
+              // Quarta coluna com ícone
+              Expanded(
+                child: Container(
+                  color: Colors.red[400],
+                  child: const Center(
+                    child: Icon(Icons.email, size: 50),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        // Última linha: Nome da turma
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                height: 80,
+                color: Colors.grey[400],
+                child: const Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    'Turma 6° semestre de engenharia de software',
+                    style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
