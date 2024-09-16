02b811e28aed316fa404b3e878635fb4b83be0d2
BrenoMOliveira <brenomoliveira03@gmail.com>
2024-09-16 11:37:19 -0300

    Criando Atividade Avaliativa Em Flutter

---
+++ layout2/lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Layout());
+  }
+}
+
+class Layout extends StatelessWidget {
+  const Layout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+                child: Container(
+              color: Colors.purple[400],
+              height: 150,
+            ))
+          ],
+        ),
+        Row(
+          children: [
+            Expanded(
+                child: Container(
+              color: Colors.green[400],
+              height: 150,
+            ))
+          ],
+        ),
+        Expanded(
+            child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(child: Container(color: Colors.blue[400])),
+            Expanded(child: Container(color: Colors.amber[400])),
+            Expanded(child: Container(color: Colors.pink[400])),
+            Expanded(child: Container(color: Colors.yellow[400])),
+          ],
+        )),
+        Row(
+          children: [
+            Expanded(
+                child: Container(
+              color: Colors.purple[400],
+              height: 150,
+            ))
+          ],
+        ),
+      ],
+    );
+  }
+}
