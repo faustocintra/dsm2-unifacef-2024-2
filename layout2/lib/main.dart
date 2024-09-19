275c2098d28cff68296bff251c58f0356f1f1eb1
marianaevangelista2004 <marianaevangelista2004@gmail.com>
2024-09-19 22:34:19 +0000

    Detalhes finais para a prova do 1bim

--- layout2/lib/main.dart
+++ layout2/lib/main.dart
@@ -66,7 +66,7 @@ class Layout extends StatelessWidget {
                 child: Text( // texto 
                   '19/09/2024', // data
                   style: TextStyle(
-                    color: Colors.orange[300], //cor
+                    color: Colors.lime[200], //cor
                     fontSize: 30, // aumentei o tamanho
                   ),
                 ),
@@ -92,24 +92,24 @@ class Layout extends StatelessWidget {
             Expanded(child: 
               Container(
                 color: Colors.amber[400],
-                child: const Center(
-                  child: Icon(Icons.settings, size: 40, color: Colors.white),
+                child: const Center( // Adiciona o ícone dentro do container e configura no centro
+                  child: Icon(Icons.settings, size: 40, color: Colors.white), // Adição dos ícones com tamanho e cor
                 ),
               ),
             ),
             Expanded(child: 
               Container(
                 color: Colors.pink[400],
-                child: const Center(
-                  child: Icon(Icons.add_alert, size: 40, color: Colors.white),
+                child: const Center( // Adiciona o ícone dentro do container e configura no centro
+                  child: Icon(Icons.add_alert, size: 40, color: Colors.white), // Adição dos ícones com tamanho e cor 
                 ),
               )
             ),
             Expanded(child: 
               Container(
                 color: Colors.red[400],
-                child: const Center(
-                  child: Icon(Icons.bolt, size: 40, color: Colors.white),
+                child: const Center( // Adiciona o ícone dentro do container e configura no centro
+                  child: Icon(Icons.bolt, size: 40, color: Colors.white), // Adição dos ícones com tamanho e cor
                 ),
               ),
             ),// coluna adicionada 
@@ -121,14 +121,14 @@ class Layout extends StatelessWidget {
           children: [
             Expanded(
                 child: Container(
-              color: Colors.grey[400],
-              height: 150,
+              color: Colors.grey[400], // cor 
+              height: 150, // altura
               child: const Align(
                 alignment: Alignment.centerRight, // Alinha o texto à direita
                 child: Text(
                   'Engenharia de software',
                 style: TextStyle(
-                  fontSize: 25,
+                  fontSize: 25, // tamanho do texto
                   fontStyle: FontStyle.italic, // Define o texto como itálico
                 ),
                 ),
