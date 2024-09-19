fe2929cf5f3b72f549e37b6bc870ef65723cd79a
marilzadias.correia <marilzadias.correia@gmail.com>
2024-09-19 22:50:56 +0000

    Prova

--- layout2/lib/main.dart
+++ layout2/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),          
     );
   }
 }
@@ -34,21 +34,21 @@ class Layout extends StatelessWidget {
   const Layout({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {            //começa a criação da tela
     return Column(
       children: [
         Row(
           children: [
             Expanded(
               child: Container(
-                color: Colors.purple[400],
+                color: Colors.purple[400],       //determina a cor do container
                 height: 150,
                 child: Center(
                   child: Text(
-                    "Thiago Correia Azarias", 
+                    "Thiago Correia Azarias",    //aqui é colocado o que vai ser escrito
                     style: TextStyle(
                     fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
+                    fontWeight: FontWeight.bold,    //aqui é onde é especificada a formatação do texto
                     color: Colors.white, 
                   ),
                 ),
@@ -60,15 +60,15 @@ class Layout extends StatelessWidget {
           children: [
             Expanded(
                 child: Container(
-              color: Colors.green[400],
+              color: Colors.red[400],     //determina a cor do backgroud do container
               height: 150,
-              child: Center(
-                  child: Text(
+              child: Center(        //Cria uma child onde tudo estará no centro
+                  child: Text(     //child com o texto
                     "19/09/2024", 
                     style: TextStyle(
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.orange[300], 
+                    fontSize: 30.0,                  //texto, alinhamento e formatação do texto
+                    fontWeight: FontWeight.bold,         
+                    color: Colors.lightGreenAccent[200],    //Cor da fonte com o tom 200
                   ),
                 ),
               ),
@@ -77,12 +77,12 @@ class Layout extends StatelessWidget {
         ),
         Expanded(
             child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+          crossAxisAlignment: CrossAxisAlignment.stretch,        //determina que as children preencham a cross axis inteira
           children: [
             Expanded(
               child: Container(
                 color: Colors.blue[400],
-                child: Icon(Icons.all_out,size: 40),
+                child: Icon(Icons.all_out,size: 40,color: Colors.white,),     //insere um ícone branco no container
                )
               
             ),
@@ -90,11 +90,11 @@ class Layout extends StatelessWidget {
               child: Container(
                 color: Colors.amber[400],
                 child: Column(
-                mainAxisAlignment: MainAxisAlignment.center, // Centraliza os ícones verticalmente
-                children: [
-                  Icon(Icons.star,size: 40), // Primeiro ícone
-                  SizedBox(height: 100),
-                  Icon(Icons.favorite_border,size: 40), // Segundo ícone
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,       //Aqui organiza a child em uma coluna e 
+                children: [                                             //faz com que elas se distribuam igualmente pela axis
+                  Icon(Icons.star,size: 40,color: Colors.white,),         
+                  SizedBox(height: 100),    //força um espaçamento entre os icones
+                  Icon(Icons.favorite_border,size: 40,color: Colors.white,), 
                 ],
              ),
               )
@@ -102,13 +102,13 @@ class Layout extends StatelessWidget {
             Expanded(
               child: Container(
                 color: Colors.pink[400],
-                child: Icon(Icons.air,size: 40),
+                child: Icon(Icons.air,size: 40,color: Colors.white,),      //insere um ícone branco no container
               )
             ),
             Expanded(
               child: Container(
                 color: Colors.lime[400],
-                child: Icon(Icons.monitor,size: 40),
+                child: Icon(Icons.monitor,size: 40,color: Colors.white,),   //insere um ícone branco no container
               )
             ),
           ],
@@ -119,18 +119,18 @@ class Layout extends StatelessWidget {
                 child: Container(
               color: Colors.brown[400],
               height: 150,
-              child: Container(
+              child: Container(           //Abre um container e o formata em formato de ROW
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.end,      //Joga os conteudos do container para o mais proximo do final do row
                   children: [
                     Text('Engenharia de software',
                     style: TextStyle(
-                    fontSize: 25.0,
-                    fontStyle: FontStyle.italic,
-                    color: Colors.white, ),
-                    
-                  ),
+                    fontSize: 25.0,              //determina o tamanho da fonte do texto
+                    fontStyle: FontStyle.italic,    //estilo da fonte do texto
+                    color: Colors.white, ),    //cor do texto
+                     ),
                   ],
+                  
                 ),
 
                 )
