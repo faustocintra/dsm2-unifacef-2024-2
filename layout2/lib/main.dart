5dacc2ce58445b3199fa5f400e97abebd038c5b4
danielduartedealmeida03 <danielduartedealmeida03@gmail.com>
2024-09-19 22:38:35 +0000

    prova 19-09

---
+++ layout2/lib/main.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Layout());
+  }
+}
+
+class Layout extends StatelessWidget {
+  const Layout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        //  Adicionando a primeira linha com nome
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.purple[400],
+                height: 150,
+                child: const Center(
+                  child: Text(
+                    'Daniel Duarte de Almeida',
+                    style: TextStyle(
+                      color: Colors.white, // cor branca na fonte
+                      fontSize: 30, // tamanho 30 da fonte
+                      fontWeight: FontWeight.bold, // negrito
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(width: 0), 
+          ],
+        ),
+        // Segunda linha com data da prova
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.green[400],
+                height: 150,
+                child: const Center(
+                  child: Text( // data da prova
+                    '19/09/2024',
+                    style: TextStyle(
+                      color: Color(0xFFE6EE9C), // cor verde limao com tonalidade 200 escrito pelo codigo da cor
+                      fontSize: 24, // fonte tamanho 24
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        // Terceira linha com ícones
+        Expanded(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // Primeira coluna com um ícone
+              Expanded(
+                child: Container(
+                  color: Colors.blue[400],
+                  child: const Icon(Icons.star, size: 50, color: Colors.white), // icone na cor branca
+                ),
+              ),
+              // Segunda coluna com um ícone
+              Expanded(
+                child: Container(
+                  color: Colors.amber[400],
+                  child: const Icon(Icons.favorite, size: 50, color: Colors.white), // icone na cor branca
+                ),
+              ),
+              // Terceira coluna com um ícone
+              Expanded(
+                child: Container(
+                  color: Colors.red[400],
+                  child: const Icon(Icons.audiotrack, size: 50, color: Colors.white), // icone na cor branca
+                ),
+              ),
+              // Quarta coluna com dois ícones
+              Expanded(
+                child: Container(
+                  color: Colors.pink[400],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Icon(Icons.home, size: 50, color: Colors.white), // icone na cor branca
+                      Icon(Icons.settings, size: 50, color: Colors.white), // icone na cor branca
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        // Quarta linha com nome da turma
+        Container(
+          color: Colors.grey[300],
+          height: 100,
+          child: const Align(
+            alignment: Alignment.centerRight,
+            child: Text( // Adicionando o nome da turma
+              '6º Semestre', 
+              style: TextStyle(
+                fontStyle: FontStyle.italic, // italico
+                fontSize: 25, // fonte 25
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
