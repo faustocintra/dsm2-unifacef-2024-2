34e490e938460ad32f920cad387691847a546676
arthursaltori <arthursaltori@gmail.com>
2024-09-19 22:36:15 +0000

    19/09 Alteração no projeto Layout2

--- layout2/lib/main.dart
+++ layout2/lib/main.dart
@@ -38,7 +38,10 @@ class Layout extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        // primeira linha com o nome completo branca, com fonte negrito tamanho 30.
+        // Primeira linha com o nome completo branca, com fonte negrito tamanho 30.
+        // Criando um Row para a primeira linha, dentro do children um expanded dentro dele
+        //criado um container como a altura e a cor de fundo
+        //apos um text para colocar um texto dentro da linha, apos isso o style do texto 
         Row(
           children: [
             Expanded(
@@ -59,8 +62,10 @@ class Layout extends StatelessWidget {
             ),
           ],
         ),
-        // segunda linha com a data da prova em cor laranja escuro, tonalidade 300,
+        // segunda linha com a data da prova em cor verde limão, tonalidade 200,
         //centralizado horizontalmente.
+        // outra Row criada com os mesmo atribuitos porem neste container a cor do fundo foi alterada
+        // e o text tambem tanto o que esta escrito como a cor dele 
         Row(
           children: [
             Expanded(
@@ -69,9 +74,9 @@ class Layout extends StatelessWidget {
                 height: 100,
                 child: Center(
                   child: Text(
-                    'Data da Prova: 19/09/2024',
+                    'Data da Prova: 19 / 09 / 2024',
                     style: TextStyle(
-                      color: Colors.orange[300], 
+                      color: Colors.lime[200], 
                       fontSize: 25,
                     ),
                   ),
@@ -81,6 +86,9 @@ class Layout extends StatelessWidget {
           ],
         ),
         // linha com quatro colunas, com icones aleatorios 
+        //aqui criamos somente um expanded sem o row porem atribuimos um child:Row para realizar que as colunas criadas apos fiquem na mesma linha
+        //cada container tem uma coluna onde em cada uma tem um background com cor diferente
+        // e em cada coluna tem um icone excessão da segunda coluna onde temos 2 icones
         Expanded(
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -135,6 +143,9 @@ class Layout extends StatelessWidget {
         ),
         // ultima linha com o nome da turma alinhado a direita em itálico, em fonte
         // tamanho 25.
+        // criamos outra row para seguir embaixo das colunas como linha e nesta linhas temos
+        // um background diferente das demais e um text diferente com um alinhamento na direita
+        // e um fontstyle mudando a a escrita para italico e e uma fonte da letra de 25px
         Row(
           children: [
             Expanded(
