0ac408491de450949a7c99da3cb4462e755d2450
stellabs.2011 <stellabs.2011@gmail.com>
2024-09-19 22:50:16 +0000

    Prova

--- layout2/lib/main.dart
+++ layout2/lib/main.dart
@@ -1,27 +1,31 @@
+// Estrutura Aplicativo Flutter
 import 'package:flutter/material.dart';
-// Importa o pacote Flutter para usar os widgets e funcionalidades
+// Importação do Pacote Flutter: importa o pacote flutter/material.dart, que contém os widgets e funcionalidades necessárias
 
 void main(){
   runApp(const MyApp());
-  // Função principal inicia o aplicativo
+  // Função Principal (main): é o ponto de entrada do aplicativo, chamamos runApp para iniciar o aplicativo com o widget MyApp
 }
 
+// Criação do Widget Principal (MyApp)
 class MyApp extends StatelessWidget{
   const MyApp({super.key});
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      title:'Flutter Demo', // título do aplicativo.
-      debugShowCheckedModeBanner: false, // Remove faixa de debug.
+      title:'Flutter Demo', // Define o título do aplicativo
+      debugShowCheckedModeBanner: false, // Remove a faixa de debug
       theme:ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple), // Define o tema do aplicativo
-        useMaterial3: true,
+        useMaterial3: true, // Utiliza o Material Design 3
       ),
-      home: const MyHomePage(title:'Flutter Demo Home Page'), // Define página inicial do aplicativo
+      home: const MyHomePage(title:'Flutter Demo Home Page'), // Define a página inicial do aplicativo
     );
   }
+  // Método build: Define a estrutura do aplicativo. MaterialApp para configurar o título, tema e a página inicial do aplicativo
 }
 
+// Definição da Página Inicial (MyHomePage)
 class MyHomePage extends StatelessWidget{
   const MyHomePage({super.key, required this.title});
   final String title;
@@ -29,17 +33,19 @@ class MyHomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return const Scaffold(body: Layout());
-    // layout da página inicial
+    // Método build: Retorna um Scaffold que define a estrutura básica da página, utilizando o widget Layout
   }
+  // Classe MyHomePage: Recebe um título como parâmetro.
 }
 
+// Criação do Layout Personalizado
 class Layout extends StatelessWidget{
   const Layout({super.key});
   @override
   Widget build(BuildContext context){
     return Column(
       children:[
-        // Primeira linha - nome completo
+        // Primeira Linha (Row): Contém um Container com o nome completo. Utilizamos Expanded para que o Container ocupe todo o espaço disponível
         Row(
           children:[
             Expanded(
@@ -48,7 +54,7 @@ class Layout extends StatelessWidget{
                 height:150,
                 child:const Center(
                   child: Text(
-                    'Stella Barbosa de Souza', // Texto primeira linha
+                    'Stella Barbosa de Souza', // Texto da primeira linha
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 30,
@@ -60,7 +66,7 @@ class Layout extends StatelessWidget{
             )
           ]
         ),
-        // Segunda linha - data da prova
+        // Segunda Linha (Row): exibe a data.
         Row(
           children: [
             Expanded(
@@ -69,9 +75,9 @@ class Layout extends StatelessWidget{
                 height: 150,
                 child: Center(
                   child: Text(
-                    'Data: 19/09/2024', // Texto segunda linha
+                    'Data: 19/09/2024', // Texto da segunda linha
                     style: TextStyle(
-                      color: Color(0xFFFFA000),
+                      color: Colors.lime[200], // Cor do texto
                       fontSize: 30,
                       fontWeight: FontWeight.bold,
                     )
@@ -81,7 +87,7 @@ class Layout extends StatelessWidget{
             )
           ]
         ),
-        // Terceira linha com quatro colunas
+        // Terceira Linha (Row): Contém quatro colunas, cada uma com um ícone diferente. Expanded e Column para organizar os ícones
         Expanded(
           child:Row(
             crossAxisAlignment:CrossAxisAlignment.stretch,
@@ -92,8 +98,8 @@ class Layout extends StatelessWidget{
                   child:Column(
                     mainAxisAlignment:MainAxisAlignment.spaceBetween,
                     children:const[
-                      Icon(Icons.star, size: 50), // Ícone de estrela preenchida
-                      Icon(Icons.star_border, size: 50), // Ícone de estrela vazia
+                      Icon(Icons.star, size: 50, color: Colors.white), // Ícone de estrela preenchida
+                      Icon(Icons.star_border, size: 50, color: Colors.white), // Ícone de estrela vazia
                     ]
                   )
                 )
@@ -102,7 +108,7 @@ class Layout extends StatelessWidget{
                 child:Container(
                   color:Colors.amber[400],
                   child:const Center(
-                    child:Icon(Icons.favorite,size: 50)// Ícone de coração
+                    child:Icon(Icons.favorite, size: 50, color: Colors.white) // Ícone de coração
                   )
                 )
               ),
@@ -110,7 +116,7 @@ class Layout extends StatelessWidget{
                 child: Container(
                   color:Colors.pink[400],
                   child:const Center(
-                    child:Icon(Icons.thumb_up,size: 50)// Ícone de polegar para cima
+                    child:Icon(Icons.thumb_up, size: 50, color: Colors.white) // Ícone de polegar para cima
                   )
                 )
               ),
@@ -118,14 +124,14 @@ class Layout extends StatelessWidget{
                 child:Container(
                   color:Colors.teal[400],
                   child:const Center(
-                    child:Icon(Icons.thumb_down,size: 50)// Ícone de polegar para baixo
+                    child:Icon(Icons.thumb_down, size: 50, color: Colors.white) // Ícone de polegar para baixo
                   )
                 )
               )
             ]
           )
         ),
-        // Última linha com curso
+        // Última Linha (Row): Exibe o curso alinhado à direita
         Row(
           children:[
             Expanded(
@@ -135,7 +141,7 @@ class Layout extends StatelessWidget{
                 child: const Align(
                   alignment: Alignment.centerRight,
                   child:Text(
-                    'Engenharia de Software', //Texto última linha
+                    'Engenharia de Software', // Texto da última linha
                     style: TextStyle(
                       fontSize: 25,
                       fontWeight: FontWeight.bold,
@@ -149,4 +155,6 @@ class Layout extends StatelessWidget{
       ]
     );
   }
+  // Método build: Column para organizar os widgets verticalmente
+  // Classe Layout: Define o layout da página inicial
 }
