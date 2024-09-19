f69c55bf585d95be774bf3b3a279cae78ce0c81d
gabrieleme05 <gabrieleme05@gmail.com>
2024-09-19 22:34:46 +0000

    Entrega Da Prova 1

--- layout2/lib/main.dart
+++ layout2/lib/main.dart
@@ -51,7 +51,8 @@ class Layout extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               )
               ),
-              
+              //Aqui eu criei outro filho(child) para conseguir colocar o texto dentro da primeira linha, coloquei os Styles, como negrito e o tamanho
+            
               
               
               )
@@ -64,14 +65,15 @@ class Layout extends StatelessWidget {
           children: [
             Expanded(
                 child: Container(
-              color: Colors.green[400],
+              color: const Color.fromARGB(255, 0, 239, 12),
               height: 150,
               alignment: Alignment.center,
               child: const Padding(padding: EdgeInsets.all(16.0),
               child: Text('Data da prova 19/09/2024', 
               style: TextStyle(
-                color: Colors.orangeAccent,
+                color: Color.fromARGB(255, 131, 77, 7),
                 fontSize: 25,
+                //Aqui usei novamente o padding, para conseguir a centralização do texto e como acima criei Child
                 )))
             ))
           ],
@@ -93,6 +95,7 @@ class Layout extends StatelessWidget {
               Expanded(child: Container(color: Colors.orange[400], child:  Icon(Icons.home, size: 50, color: Colors.white),)),
               Expanded(child: Container(color: Colors.brown[400], child: Icon(Icons.settings, size: 50, color: Colors.white) ,)),
               Expanded(child: Container(color: Colors.cyan[400],child: Icon(Icons.person, size: 50, color: Colors.white),)),
+            //Aqui foi umas das partes mais difíceis, porque tinha que abrir dois child e depois colocar o os icones, com tamanho e cores
             ],
           ),
         ),
@@ -103,11 +106,12 @@ class Layout extends StatelessWidget {
               height: 100,
               color: Colors.grey[400],
               alignment: Alignment.centerRight,
-              child: const Padding(padding: EdgeInsets.all(16.0),
+              child: const Padding(padding: EdgeInsets.all(23.0),
               child: Text('Sexto semestre Engenharia de Software', 
               style: TextStyle(
                 fontSize: 19.5,
                 fontStyle: FontStyle.italic
+                //Abri um novo children, que abrange o child, para colocar informações na ultima linha
                 )))
             )
           ],
