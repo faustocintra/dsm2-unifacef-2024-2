93a8d65a2fb67ed3047ee37459a9b63df0467b59
marilzadias.correia <marilzadias.correia@gmail.com>
2024-09-26 23:14:30 +0000

    (26/09) Inicio do projeto Facefpalooza

---
+++ facefpalooza/lib/widgets/act_list.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../data/act.dart';
+
+class ActList extends StatelessWidget{
+  const ActList({super.key});
+  
+  final dados = lineup;
+
+  @override
+  Widget build (BuildContext context) {
+    return ListView.builder(
+      itemCount: data.length,
+      itemBuilder: (context, index){
+        final item = data[index];
+
+        return ListTile(
+          leading: CircleAvatar(
+            child: Text("${item.day}")
+          ),
+          title: Text(item.name),
+          subtitle: Wrap(
+            spacing: 8,
+            runSpacing: 4,
+            children: item.tags.map(
+              (tag) => Chip(label: Text("#$tag"))
+
+            ).toList(),
+          )
+        )
+      }
+    )
+  }
+}
