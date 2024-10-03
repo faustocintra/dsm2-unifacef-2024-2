54729f532bbcc6bb2dcb57e001573ba96ea99bd5
pedropmartiniano <pedropmartiniano@gmail.com>
2024-10-03 23:26:25 +0000

    aula 03/10

---
+++ facefpalooza/lib/widget/acts_list.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import '../data/acts.dart';
+
+class ActList extends StatelessWidget {
+  const ActList({super.key});
+
+  final data = lineup;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (context, index) {
+          final item = data[index];
+          return ListTile(
+              leading: CircleAvatar(child: Text("${item.day}")),
+              title: Text(item.name),
+              subtitle: Wrap(
+                  spacing: 8,
+                  runSpacing: 4,
+                  children: item.tags
+                      .map((tag) => Chip(label: Text("#$tag")))
+                      .toList()));
+        });
+  }
+}
