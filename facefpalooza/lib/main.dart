084c84ff7a93c69cccdfb138006436b0397b18bd
arthursaltori <arthursaltori@gmail.com>
2024-10-10 23:26:02 +0000

    (10/10) Integração do Firebase com o Flutter

--- facefpalooza/lib/main.dart
+++ facefpalooza/lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'widgets/act_list.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+void main() async {
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
