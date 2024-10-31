9ce21cee15acec82d088997ca9258f1a3a1f2092
arthursaltori <arthursaltori@gmail.com>
2024-10-31 23:34:52 +0000

    (31/10) Correção do erro br.facef.facefpalooza

--- facefpalooza/lib/main.dart
+++ facefpalooza/lib/main.dart
@@ -3,14 +3,13 @@ import 'widgets/act_list.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
