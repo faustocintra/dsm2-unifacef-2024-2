e80e941031605491564cd195c7752fa432a679b0
gabrieleme05 <gabrieleme05@gmail.com>
2024-10-31 23:21:58 +0000

    FacefPalooza (31/10/2024)

--- facefpalooza/lib/firebase_options.dart
+++ facefpalooza/lib/firebase_options.dart
@@ -64,7 +64,7 @@ class DefaultFirebaseOptions {
     messagingSenderId: '513251127780',
     projectId: 'facefpalooza-264d4',
     storageBucket: 'facefpalooza-264d4.appspot.com',
-    iosBundleId: 'com.example.facefpalooza',
+    iosBundleId: 'br.facef.facefpalooza',
   );
 
   static const FirebaseOptions macos = FirebaseOptions(
@@ -73,7 +73,7 @@ class DefaultFirebaseOptions {
     messagingSenderId: '513251127780',
     projectId: 'facefpalooza-264d4',
     storageBucket: 'facefpalooza-264d4.appspot.com',
-    iosBundleId: 'com.example.facefpalooza',
+    iosBundleId: 'br.facef.facefpalooza',
   );
 
   static const FirebaseOptions windows = FirebaseOptions(
