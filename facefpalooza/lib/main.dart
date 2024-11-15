3d393f24cb70f0e19bba5698cd052b8ad57eb70c
maria.marcela.sousa <maria.marcela.sousa@gmail.com>
2024-11-15 14:39:45 +0000

    Trabalho 2º bimestre

---
+++ facefpalooza/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'widgets/act_list.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Facefpalooza',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      themeMode:
+          ThemeMode.system, // Permite alternar entre os temas automaticamente
+      home: const MyHomePage(title: 'Facefpalooza'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: const ActList(),
+    );
+  }
+}
---
+++ facefpalooza/lib/widgets/act_list.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../data/act.dart';
+
+class ActList extends StatelessWidget {
+  const ActList({super.key});
+
+  final data = lineup;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('acts')
+          .orderBy('day') // Ordena pelo dia
+          .orderBy('relevance') // Ordena pela relevância
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (!snapshot.hasData) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        var list = snapshot.data?.docs ?? [];
+
+        return ListView(
+          children: list.map<Widget>((act) {
+            return ListTile(
+              leading: CircleAvatar(child: Text("${act['day']}")),
+              title: Text(
+                act['name'],
+                style: TextStyle(
+                  fontSize: 20, // Aumenta o tamanho da fonte
+                  fontWeight: FontWeight.bold, // Deixa o texto em negrito
+                ),
+              ),
+              subtitle: Wrap(
+                spacing: 8,
+                runSpacing: 4,
+                children: act['tags']
+                    .map<Widget>((tag) => Chip(
+                          label: Text("#$tag"),
+                          backgroundColor:
+                              Colors.blueGrey, // Cor de fundo do Chip
+                        ))
+                    .toList(),
+              ),
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+}
