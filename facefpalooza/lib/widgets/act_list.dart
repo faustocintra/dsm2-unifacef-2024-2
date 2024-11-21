edd57c095ed67854f1cdf8b6215fbd002ba6f5a1
iaat.franca <iaat.franca@gmail.com>
2024-11-21 22:09:01 +0000

    alterações para o trabalho

--- facefpalooza/lib/widgets/act_list.dart
+++ facefpalooza/lib/widgets/act_list.dart
@@ -7,29 +7,43 @@ class ActList extends StatelessWidget {
 
   final data = lineup;
 
+//Foi criado do Widget e feita suas alterações de cores
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('acts').snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(child: CircularProgressIndicator());
-          }
+      stream: FirebaseFirestore.instance.collection('acts').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (!snapshot.hasData) {
+          return const Center(child: CircularProgressIndicator());
+        }
 
-          var list = snapshot.data?.docs ?? [];
+        var list = snapshot.data?.docs ?? [];
 
-          return ListView(
-              children: list.map<Widget>((act) {
+        return ListView(
+          children: list.map<Widget>((act) {
             return ListTile(
-                leading: CircleAvatar(child: Text("${act['day']}")),
-                title: Text(act['name']),
-                subtitle: Wrap(
-                    spacing: 8,
-                    runSpacing: 4,
-                    children: act['tags']
-                        .map<Widget>((tag) => Chip(label: Text("#$tag")))
-                        .toList()));
-          }).toList());
-        });
+              leading: CircleAvatar(child: Text("${act['day']}")),
+              title: Text(
+                act['name'],
+                style: TextStyle(fontWeight: FontWeight.bold), // Colocando o nome em negrito
+              ),
+             subtitle: Wrap(
+             spacing: 8,
+             runSpacing: 4,
+             children: act['tags']
+      .map<Widget>((tag) => Chip(
+  label: Text("#$tag"),
+  backgroundColor: Theme.of(context).colorScheme.secondary, // Usando a cor primária do tema
+  labelStyle: TextStyle(color: Colors.white), // Cor do texto dentro do Chip
+)
+) // Criando um Chip para cada tag
+      .toList(),
+),
+
+            );
+          }).toList(),
+        );
+      },
+    );
   }
 }
