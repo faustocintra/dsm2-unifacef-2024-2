a786b561f12b9e3c9e3e19c7da34f2ecf44b67e6
BrenoMOliveira <brenomoliveira03@gmail.com>
2024-11-21 08:44:48 -0300

    Conectando ao firebase, adicionando modo escuro

---
+++ facefpalooza/lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'widgets/act_list.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.system;
+
+  void _toggleTheme() {
+    setState(() {
+      _themeMode =
+          _themeMode == ThemeMode.dark ? ThemeMode.light : ThemeMode.dark;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Facefpalooza',
+      //tema dinamico de acordo com sistema
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.light,
+        ),
+        useMaterial3: true,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      themeMode: _themeMode,
+      home: MyHomePage(title: 'Facefpalooza', toggleTheme: _toggleTheme),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title, required this.toggleTheme});
+
+  final String title;
+  final VoidCallback toggleTheme;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+        // botao para alterar entre modo claro e escuro
+        actions: [
+          IconButton(
+            icon: Icon(Icons.brightness_6),
+            onPressed: widget.toggleTheme, // Alterna entre claro e escuro
+          ),
+        ],
+      ),
+      body: const ActList(),
+    );
+  }
+}
--- layout2/lib/main.dart
+++ layout2/lib/main.dart
@@ -1,137 +1,114 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Exercício de Layout',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Exercício de Layout'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(body: Layout());
-  }
-}
-
-class Layout extends StatelessWidget {
-  const Layout({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            Expanded(
-                child: Container(
-              color: Colors.greenAccent[400],
-              height: 150,
-              alignment: Alignment.center, //alinhamento do conteudo
-              padding: const EdgeInsets.all(16),
-              child: const Text( // nome completo e estilo do nome
-                'Breno Martins Oliveira',
-                style: TextStyle(
-                  fontSize: 30, //tamanho da fonte
-                  fontWeight: FontWeight.bold, //negrito
-                  color: Colors.white,
-                ),
-                textAlign: TextAlign.center, //alinhamento do texto
-              ),
-            ))
-          ],
+      home: Scaffold(
+        backgroundColor: Colors.black, // Fundo da tela
+        appBar: AppBar(
+          title: const Text('Exercício de Layout'),
         ),
-        Row(
+        body: Column(
           children: [
-            Expanded(
-                child: Container( 
-              color: Colors.blueGrey[400],
-              height: 150,
-              alignment: Alignment.center, //alinhamento do conteudo
+            Container(
+              color: Colors.lightBlue[200],
+              alignment: Alignment.center,
               padding: const EdgeInsets.all(16),
-              child: Text( //data da prova e e estilo da data
-                '19/09/2024',
+              child: const Text(
+                'Eder Junior Alves Silva',
                 style: TextStyle(
                   fontSize: 30,
-                  color: Colors.deepOrange[300], //cor
-                ),
-                textAlign: TextAlign.center, //alinhamento do texto
-              ),
-            ))
-          ],
-        ),
-        Expanded(
-            child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(child: Container(
-              color: Colors.purple[400],
-                child: const Column( //coluna com dois icones
-                  children: [
-                    Icon(Icons.android, size: 50, color: Colors.black), // icone superior
-                    Spacer(), // espaçamento dos ícones
-                    Icon(Icons.smartphone, size: 50, color: Colors.black), // icone inferior
-                  ],
-                )
-              )),
-            Expanded(child: Container(
-              color: Colors.cyan[400],
-                child: const Center( //coluna com um icone
-                  child: Icon(Icons.code, size: 50, color: Colors.black), // icone
-                ),
-              )),
-            Expanded(child: Container(
-              color: Colors.teal[400],
-                child: const Center( //coluna com um icone
-                  child: Icon(Icons.code_off, size: 50, color: Colors.black), // icone
-                ),
-              )),
-            Expanded(child: Container( 
-              color: Colors.lime[400],
-                child: const Center( //coluna com um icone
-                  child: Icon(Icons.devices, size: 50, color: Colors.black), // icone
-                ),
-              )),
-          ],
-        )),
-        //linha adicional
-        Row(
-          children: [
-            Expanded(
-                child: Container(
-              color: Colors.red[400],
-                            height: 150,
-              alignment: Alignment.centerRight, //alinhamento do conteudo
-              padding: const EdgeInsets.all(16),
-              child: const Text( //nome da turma e estilização do texto
-                'Engenharia de Software',
-                style: TextStyle(
-                  fontSize: 25, //tamanho da fonte
-                  fontStyle: FontStyle.italic, //estilo da fonte
+                  fontWeight: FontWeight.bold, // Negrito
                   color: Colors.white,
                 ),
-                textAlign: TextAlign.right, //alinhamento do texto
+                textAlign: TextAlign.center,
               ),
-            ))
+            ),
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(16),
+              child: Text(
+                'Data da Prova: 19/09/2024',
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.lightGreen[200], // Laranja escuro tonalidade 300
+                ),
+                textAlign: TextAlign.center, 
+              ),
+            ),
+            // Quatro colunas com ícones e cores de fundo em tonalidade 400
+            Expanded(
+              child: Row(
+                children: [
+                  // Primeira coluna com dois ícones
+                  Expanded(
+                    child: Container(
+                      color: Colors.purple[400],
+                      child: const Column(
+                        children: [
+                          Icon(Icons.code, size: 50, color: Colors.white), // Ícone (superior)
+                          Spacer(), // Espaçamento entre os ícones
+                          Icon(Icons.bug_report, size: 50, color: Colors.white), // Ícone (inferior)
+                        ],
+                      ),
+                    ),
+                  ),
+                  // Segunda coluna com um ícone
+                  Expanded(
+                    child: Container(
+                      color: Colors.teal[400],
+                      child: const Center(
+                        child: Icon(Icons.computer, size: 50, color: Colors.white), // Ícone de computador
+                      ),
+                    ),
+                  ),
+                  // Terceira coluna com um ícone
+                  Expanded(
+                    child: Container(
+                      color: Colors.orange[400],
+                      child: const Center(
+                        child: Icon(Icons.developer_mode, size: 50, color: Colors.white), // Ícone de desenvolvedor
+                      ),
+                    ),
+                  ),
+                  // Quarta coluna com um ícone
+                  Expanded(
+                    child: Container(
+                      color: Colors.green[400],
+                      child: const Center(
+                        child: Icon(Icons.memory, size: 50, color: Colors.white), // Ícone de memória
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // Última linha: Nome da turma
+            Container(
+              color: Colors.blue[400], // Cor de fundo para a última linha
+              alignment: Alignment.centerRight, // Alinhamento à direita
+              padding: const EdgeInsets.all(16),
+              child: const Text(
+                'Turma: Engenharia de Software',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontStyle: FontStyle.italic, // itálico
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.right, // Alinhamento à direita
+              ),
+            ),
           ],
         ),
-      ],
+      ),
     );
   }
 }
